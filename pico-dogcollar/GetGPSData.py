63794f9ece8e1e761e4881af3136aeebbf9007f7
RHartnagel <rhartnagel13@gmail.com>
2025-01-21 12:37:44 -0700

    made location info a class and created and returned it for each parse

--- pico-dogcollar/GetGPSData.py
+++ pico-dogcollar/GetGPSData.py
@@ -4,6 +4,17 @@ import time
 
 uart = UART(0, 9600)
 
+class LocationInfo:
+    def __init__(self, time, lat, dLat, long, dLong):
+        self.time = time
+        self.lat = lat
+        self.dLat = dLat
+        self.long = long
+        self.dLong = dLong
+
+    def print(self):
+        print(f"Time: {self.time}, Lat: {self.lat} {self.dLat}, Long: {self.long} {self.dLong}")
+
 def filterLines(raw_line):
     if raw_line:
         try:
@@ -29,11 +40,14 @@ def convertTime(rawTime):
 
 def parseLine(lines):
     if lines[0] == "$GNGGA" and all(lines[i] for i in [1, 2, 3, 4, 5]):
-        print(f"Time: {lines[1]}, Lat: {lines[2]} {lines[3]}, Long: {lines[4]} {lines[5]}")
+        info = LocationInfo(lines[1], lines[2], lines[3], lines[4], lines[5])
+        return info
     elif lines[0] == "$GNGLL" and all(lines[i] for i in [1, 2, 3, 4, 5]):
-        print(f"Time: {lines[5]}, Lat: {lines[1]} {lines[2]}, Long: {lines[3]} {lines[4]}")
+        info = LocationInfo(lines[5], lines[1], lines[2], lines[3], lines[4])
+        return info
     elif lines[0] == "$GNRMC" and all(lines[i] for i in [1, 3, 4, 5, 6]):
-            print(f"Time: {lines[1]}, Lat: {lines[3]} {lines[4]}, Long: {lines[5]} {lines[6]}")
+        info = LocationInfo(lines[1], lines[3], lines[4], lines[5], lines[6])
+        return info
     else:
         raise Exception("Bad Input/Warming Up")
 
@@ -41,7 +55,7 @@ def createSendPacket(lat, long):
     sendLat = int(lat * 1e5)
     sendLong = int(long * 1e5)
     timeNow = int(time.time())
-    return struct.pack("!iiI", sendLat, sendLat, int(timeNow))
+    return struct.pack("!iiI", sendLat, sendLong, int(timeNow))
 
 while True:
     time.sleep(0.1)
@@ -50,7 +64,8 @@ while True:
     if not truth:
         continue
     try:
-        parseLine(message)
+        info = parseLine(message)
     except Exception as e:
         print(f"got error: {e}")
         continue
+    info.print()
